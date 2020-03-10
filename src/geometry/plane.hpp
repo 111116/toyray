68cd0a63837371a3e417f15cf5d0f4b4b9c796c8
bilibili <fenggaobuke@gmail.com>
2020-03-11 00:14:21 +0800

    implement plane

---
+++ src/geometry/plane.hpp
@@ -0,0 +1,43 @@
+#ifndef PLANE_H
+#define PLANE_H
+
+#include "geometry.hpp"
+
+// infinite plane of ax+by+cz=d where (a,b,c) is normalized
+
+class Plane : public Primitive
+{
+protected:
+    vec3f abc;
+    float d;
+public:
+    Plane(const vec3f& normal, float d) : abc(normal), d(d) {}
+
+    bool intersect(const Ray& ray, point* result) const {
+        float lambda = (d - dot(abc, ray.origin)) / dot(abc, ray.dir);
+        if (lambda >= 0) {
+        	*result = ray.atParam(lambda);
+        	return true;
+        }
+        return false;
+    }
+
+    vec3f Ns(const point& p) const
+    {
+        return abc;
+    }
+
+    vec3f Ng(const point& p) const
+    {
+        return abc;
+    }
+
+    AABox boundingVolume() const
+    {
+        return AABox::infAAB;
+    }
+};
+
+#endif //PLANE_H
+		
+
