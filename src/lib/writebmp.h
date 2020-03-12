ea279e5f48d9809369b5556e2eeed07732891187
bilibili <fenggaobuke@gmail.com>
2020-03-12 18:31:43 +0800

    fix bmp not clamping color overflow

--- src/lib/writebmp.h
+++ src/lib/writebmp.h
@@ -30,7 +30,7 @@ void writeBMP(const char* filename, float* data, int W, int H)
 {
 	char bytedata[W*H*3];
 	for (int i=0; i<W*H*3; ++i)
-		bytedata[i] = (data[i]>1? 1: data[i]) *255;
+		bytedata[i] = std::min(255, int((data[i]>1? 1: data[i]) *255));
 	return writeBMP(filename, bytedata, W, H);
 }
 
