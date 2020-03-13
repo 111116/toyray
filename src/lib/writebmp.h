5ad7f4405d299a7638d95996efa8ee62fd6de9d1
bilibili <fenggaobuke@gmail.com>
2020-03-14 00:18:23 +0800

    fix bmp overflow

--- src/lib/writebmp.h
+++ src/lib/writebmp.h
@@ -33,7 +33,7 @@ void writeBMP(const char* filename, float* data, int W, int H, float outputgamma
 {
 	char bytedata[W*H*3];
 	for (int i=0; i<W*H*3; ++i)
-		bytedata[i] = std::min(255, int(data[i]>1? 1: std::pow(data[i],1.0/outputgamma) *255));
+		bytedata[i] = std::min(255, int(std::pow(data[i],1.0/outputgamma) *255));
 	return writeBMP(filename, bytedata, W, H);
 }
 
