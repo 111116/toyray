c611764a9597fdc7227a0956eee10876752a250f
bilibili <fenggaobuke@gmail.com>
2020-03-16 17:55:10 +0800

    geometry: add support for triangle

--- src/geometry/geometrytypes.hpp
+++ src/geometry/geometrytypes.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include "triangle.hpp"
 #include "trianglemesh.hpp"
 #include "sphere.hpp"
 #include "plane.hpp"
 #include "quad.hpp"
 #include "cube.hpp"
-#include "../lib/json.hpp"
+#include "../jsonutil.hpp"
 #include "../transformparser.hpp"
 
 Primitive* newPrimitive(const Json& conf)
@@ -28,6 +29,31 @@ Primitive* newPrimitive(const Json& conf)
 		if (conf.find("transform") != conf.end())
 			trans = parseTransform(conf["transform"]);
 		shape = new Quad(trans);
+	}
+	if (conf["type"] == "triangle") {
+		vec3f v1,v2,v3,n1,n2,n3;
+		bool recompute_normal = true;
+		v1 = json2vec3f(conf["vertices"][0]);
+		v2 = json2vec3f(conf["vertices"][1]);
+		v3 = json2vec3f(conf["vertices"][2]);
+		if (conf.find("normals") != conf.end()) {
+			recompute_normal = false;
+			n1 = json2vec3f(conf["normals"][0]);
+			n2 = json2vec3f(conf["normals"][1]);
+			n3 = json2vec3f(conf["normals"][2]);
+		}
+		vec2f t1,t2,t3;
+		if (conf.find("uvs") != conf.end()) {
+			t1 = json2vec2f(conf["uvs"][0]);
+			t2 = json2vec2f(conf["uvs"][1]);
+			t3 = json2vec2f(conf["uvs"][2]);
+		}
+		Triangle* tshape = new Triangle(v1,v2,v3,t1,t2,t3,n1,n2,n3);
+		if (conf.find("recompute_normals") != conf.end())
+			recompute_normal = conf["recompute_normals"];
+		if (recompute_normal)
+			tshape->recompute_normal();
+		shape = tshape;
 	}
 	if (shape == NULL) {
 		throw std::runtime_error("Unrecognized Geometric type: " + std::string(conf["type"]));
