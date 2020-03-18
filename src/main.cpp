995ce58b61fdfb7906825f031414314160c53629
bilibili <fenggaobuke@gmail.com>
2020-03-18 14:56:20 +0800

    separate renderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-// ray casting
 
 #include <assert.h>
 
@@ -8,89 +7,21 @@
 
 #include <algorithm>
 #include <unordered_map>
-#include <vector>
 #include <fstream>
 #include <ctime>
 #include <chrono>
 
 #include "color.h"
 #include "lib/saveexr.h"
-#include "util/jsonutil.hpp"
-#include "util/filepath.hpp" // model directory
 #include "lib/consolelog.hpp"
+#include "util/jsonutil.hpp"
+#include "util/filepath.hpp"
 
-#include "object.hpp"
-#include "lights/lighttypes.hpp"
-#include "accelarator/bvhsah.hpp"
-#include "accelarator/bruteforce.hpp"
-#include "samplers/randomsampler.hpp"
+#include "renderer.hpp"
 #include "film.hpp"
-#include "cameras/camera.hpp"
-
-
-
-std::vector<Object*> objects;
-std::vector<Light*> samplable_lights;
-std::vector<Light*> global_lights;
-Accelarator* acc;
-std::unordered_map<std::string, BSDF*> bsdf;
-int max_bounces = 16;
-// TODO don't use fixed eps
-const float geoEPS = 1e-3;
-
-
-Color normal(Ray ray, Sampler&) {
-	HitInfo hit = acc->hit(ray);
-	if (!hit) {
-		return Color();
-	}
-	return Color(0.5,0.5,0.5) + 0.5 * hit.Ns;
-}
-
-
-// sample radiance using ray casting
-Color radiance(Ray ray, Sampler& sampler)
-{
-	Color through(1);
-	Color result;
-	bool lastDirac = true; // accept all light sources if directly into camera
-	for (int nbounce = 0;; ++nbounce)
-	{
-		HitInfo hit = acc->hit(ray);
-		if (!hit) {
-			for (Light* l: global_lights)
-				result += through * l->radiance(ray);
-			break;
-		}
-		if (hit.object->emission) {
-			// TODO
-			if (lastDirac || !hit.object->emission->samplable)
-				result += through * hit.object->emission->radiance(ray);
-		}
-		BSDF* bsdf = hit.object->bsdf;
-		if (!bsdf) break;
-		if (nbounce == max_bounces) break;
-		// direct light (local) // TODO
-		for (Light* l: samplable_lights) {
-			vec3f dirToLight;
-			float dist;
-			Color irr = l->sampleIrradiance(hit.p, dirToLight, dist, sampler);
-			dist -= geoEPS; // cancel amount that ray origin is moved forward
-			// shadow ray test
-			Ray shadowray(hit.p + geoEPS * dirToLight, dirToLight);
-			HitInfo shadowhit = acc->hit(shadowray);
-			if (!shadowhit || norm(shadowhit.p - shadowray.origin) > (1-geoEPS) * dist)
-				result += through * irr * fabs(dot(hit.Ns, dirToLight)) * bsdf->f(-ray.dir, dirToLight, hit.Ns, hit.Ng);
-		}
-		// indirect light (bsdf importance sampling)
-		vec3f newdir;
-		Color f = bsdf->sample_f(-ray.dir, newdir, hit.Ns, hit.Ng, lastDirac, sampler); // already scaled by 1/pdf
-		through *= fabs(dot(newdir, hit.Ns)) * f;
-		if (through == vec3f() || !(sqrlen(through) < 1e20)) break;
-		ray = Ray(hit.p + geoEPS*newdir, newdir);
-	}
-	return result;
-}
+#include "lights/lighttypes.hpp"
+#include "accelarator/bruteforce.hpp"
+#include "accelarator/bvhsah.hpp"
 
 
 
@@ -101,7 +32,7 @@ void welcome(int argc, char* argv[]) {
 #ifdef THREADED
 #pragma omp parallel
 	{
-	#pragma omp single
+#pragma omp single
 		console.info(omp_get_num_threads(), "THREADS");
 	}
 #else
@@ -114,44 +45,38 @@ void welcome(int argc, char* argv[]) {
 }
 
 
-void loadMaterials(const Json& conf) {
+std::unordered_map<std::string, BSDF*> bsdf;
+
+
+void loadObjects(const Json& conf, Renderer& renderer)
+{
 	for (auto o: conf["bsdfs"]) {
 		bsdf[o["name"]] = newMaterial(o);
 	}
-}
-void loadPrimitives(const Json& conf)
-{
 	instantiateGeometry(conf);
 	for (auto o: conf["primitives"]) {
-		// if (o["type"] == "infinite_sphere") {
-		// 	// assume infinite_sphere will only be used for light probes
-		// 	globalLightProbe = new LightProbe(getpath(o["emission"]).c_str());
-		// 	// TODO currently light probes are not samplable
-		// }
-		// else {
-			Object* newobj;
-			if (bsdf.find(std::string(o["bsdf"])) == bsdf.end())
-				console.warn("Undefined BSDF", o["bsdf"]);
-			newobj = new Object(o, bsdf[o["bsdf"]]);
-			objects.push_back(newobj);
-		// }
+		Object* newobj;
+		if (bsdf.find(std::string(o["bsdf"])) == bsdf.end())
+			console.warn("Undefined BSDF", o["bsdf"]);
+		newobj = new Object(o, bsdf[o["bsdf"]]);
+		renderer.objects.push_back(newobj);
 	}
 }
 
-void loadSources(const Json& conf) {
+void loadSources(const Json& conf, Renderer& renderer) {
 	// add light sources
-	for (auto o: objects) {
+	for (auto o: renderer.objects) {
 		if (o->emission && o->emission->samplable)
-			samplable_lights.push_back(o->emission);
+			renderer.samplable_lights.push_back(o->emission);
 	}
 	if (conf.find("emissions") != conf.end()) {
 		for (auto o: conf["emissions"]) {
 			Light* light = newLight(o);
 			if (light->samplable) {
-				samplable_lights.push_back(light);
+				renderer.samplable_lights.push_back(light);
 			}
 			else {
-				global_lights.push_back(light);
+				renderer.global_lights.push_back(light);
 			}
 		}
 	}
@@ -170,7 +95,6 @@ std::vector<std::string> getOutputFiles(const Json& conf)
 	return v;
 }
 
-
 int main(int argc, char* argv[])
 {
 	try {
@@ -182,34 +106,29 @@ int main(int argc, char* argv[])
 		// load scene conf file
 		Json conf;
 		fin >> conf;
-		loadMaterials(conf);
-		loadPrimitives(conf);
-		loadSources(conf);
-		acc = new BVH(objects);
+		Renderer renderer;
+		loadObjects(conf, renderer);
+		loadSources(conf, renderer);
+		renderer.acc = new BVH(renderer.objects);
 		if (conf.find("integrator") != conf.end()) {
 			if (conf["integrator"].find("max_bounces") != conf["integrator"].end())
-				max_bounces = conf["integrator"]["max_bounces"];
+				renderer.max_bounces = conf["integrator"]["max_bounces"];
 		}
-		int nspp = conf["renderer"]["spp"];
+		renderer.nspp = conf["renderer"]["spp"];
 		Camera* camera = newCamera(conf["camera"]);
+		renderer.camera = camera;
 
 		Film film(camera->resx, camera->resy);
-		console.info("Rendering at", camera->resx, 'x', camera->resy, 'x', nspp, "spp");
+		console.info("Rendering at", camera->resx, 'x', camera->resy, 'x', renderer.nspp, "spp");
 		// start rendering
 		console.time("Rendered");
 		int line_finished = 0;
 #pragma omp parallel for schedule(dynamic)
 		for (int y=0; y<camera->resy; ++y) {
 			for (int x=0; x<camera->resx; ++x) {
-				Color res;
-				for (int i=0; i<nspp; ++i) {
-					Sampler* sampler = new RandomSampler();
-					vec2f uv = (vec2f(x,y) + sampler->get2f()) * vec2f(1.0/camera->resx, 1.0/camera->resy);
-					Ray ray = camera->sampleray(uv);
-					Color tres = radiance(ray, *sampler);
-					/*if (norm(tres)<1e8)*/ res += tres;
-				}
-				film.setPixel(x, y, res/nspp);
+				vec2f pixelsize = vec2f(1.0/camera->resx, 1.0/camera->resy);
+				vec2f pixelpos = vec2f(x,y) * pixelsize;
+				film.setPixel(x, y, renderer.render(pixelpos, pixelsize));
 			}
 #pragma omp critical
 			// report progress
---
+++ src/renderer.cpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "renderer.hpp"
+#include "samplers/randomsampler.hpp"
+
+
+Color Renderer::render(const vec2f& pixelpos, const vec2f& pixelsize) {
+	Color res;
+	for (int i=0; i<nspp; ++i) {
+		Sampler* sampler = new RandomSampler();
+		vec2f uv = pixelpos + sampler->get2f() * pixelsize;
+		Ray ray = camera->sampleray(uv);
+		Color tres = radiance(ray, *sampler);
+		/*if (norm(tres)<1e8)*/ res += tres;
+	}
+	return res / nspp;
+}
+
+
+Color Renderer::normal(Ray ray, Sampler&) {
+	HitInfo hit = acc->hit(ray);
+	if (!hit) {
+		return Color();
+	}
+	return Color(0.5,0.5,0.5) + 0.5 * hit.Ns;
+}
+
+
+// path tracer
+Color Renderer::radiance(Ray ray, Sampler& sampler)
+{
+	Color through(1);
+	Color result;
+	bool lastDirac = true; // accept all light sources if directly into camera
+	for (int nbounce = 0;; ++nbounce)
+	{
+		HitInfo hit = acc->hit(ray);
+		if (!hit) {
+			for (Light* l: global_lights)
+				result += through * l->radiance(ray);
+			break;
+		}
+		if (hit.object->emission) {
+			// TODO
+			if (lastDirac || !hit.object->emission->samplable)
+				result += through * hit.object->emission->radiance(ray);
+		}
+		BSDF* bsdf = hit.object->bsdf;
+		if (!bsdf) break;
+		if (nbounce == max_bounces) break;
+		// direct light (local) // TODO
+		for (Light* l: samplable_lights) {
+			vec3f dirToLight;
+			float dist;
+			Color irr = l->sampleIrradiance(hit.p, dirToLight, dist, sampler);
+			dist -= geoEPS; // cancel amount that ray origin is moved forward
+			// shadow ray test
+			Ray shadowray(hit.p + geoEPS * dirToLight, dirToLight);
+			HitInfo shadowhit = acc->hit(shadowray);
+			if (!shadowhit || norm(shadowhit.p - shadowray.origin) > (1-geoEPS) * dist)
+				result += through * irr * fabs(dot(hit.Ns, dirToLight)) * bsdf->f(-ray.dir, dirToLight, hit.Ns, hit.Ng);
+		}
+		// indirect light (bsdf importance sampling)
+		vec3f newdir;
+		Color f = bsdf->sample_f(-ray.dir, newdir, hit.Ns, hit.Ng, lastDirac, sampler); // already scaled by 1/pdf
+		through *= fabs(dot(newdir, hit.Ns)) * f;
+		if (through == vec3f() || !(sqrlen(through) < 1e20)) break;
+		ray = Ray(hit.p + geoEPS*newdir, newdir);
+	}
+	return result;
+}
