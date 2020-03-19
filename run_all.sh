b7c58b094f552b111f7bbd6db6d78b2549d8614d
bilibili <fenggaobuke@gmail.com>
2020-03-19 10:23:24 +0800

    add test script

---
+++ run_all.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# If project not ready, generate cmake file.
+if [[ ! -d build ]]; then
+    mkdir -p build
+    cd build
+    cmake ..
+    cd ..
+fi
+
+# Build project.
+cd build
+make -j
+cd ..
+mkdir bin
+cp build/toyray bin/PA1
+
+# Run all testcases. 
+mkdir -p output
+mkdir -p hdroutput
+for ((i=1; i<=7; ++i))
+do
+	echo -e "\033[0;32mTestcase $i running...\033[0m"
+	if bin/PA1 testcases/$i.json
+	then
+		echo -e "\033[0;32mTestcase $i successfully exited.\033[0m"
+		mv $i.exr hdroutput/
+		mv $i.bmp output/
+	else
+		echo -e "\033[1;33mTestcase $i failed.\033[0m"
+	fi
+	echo
+done
