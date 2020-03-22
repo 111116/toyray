a6af5d0d6e37cfab0640a646f22c2de523c8492b
bilibili <fenggaobuke@gmail.com>
2020-03-22 17:14:17 +0800

    separate dielectric formula

--- src/bsdfs/dielectric.hpp
+++ src/bsdfs/dielectric.hpp
@@ -15,33 +15,38 @@ public:
 		flags = Type(DIRAC | TRANSMIT | REFLECT);
 	}
 
-	Color sample_f(const vec3f& wo, vec3f& wi, float& pdf, Sampler& sampler) const override
+	// Fresnel's law for dielectrics; cosi>0
+	static inline float reflectivity(float cosi, float ior)
 	{
-		wi = vec3f(-wo.x, -wo.y, wo.z);
-		float ior = wo.z>0? IOR: 1/IOR;
-		float sini = sqrtf(wo.x*wo.x+wo.y*wo.y);
+		float sini = sqrtf(1-cosi*cosi);
 		float sint = sini / ior;
-		if (fabs(sint) >= 1) { // total reflect
-			wi = vec3f(-wo.x, -wo.y, wo.z);
-			pdf = 1;
-			return 1 / fabs(wi.z);
-		}
-		float cosi = fabs(wo.z);
+		if (sint >= 1) return 1; // total reflect
 		float cost = sqrtf(1-sint*sint);
 		float r1 = (ior*cosi - cost) / (ior*cosi + cost);
 		float r2 = (cosi - ior*cost) / (cosi + ior*cost);
-		float r = 0.5 * (r2*r2 + r1*r1); // reflect ratio
-		if (sampler.get1f()<r) {
+		return 0.5 * (r2*r2 + r1*r1);
+	}
+
+	Color sample_f(const vec3f& wo, vec3f& wi, float& pdf, Sampler& sampler) const override
+	{
+		float ior = wo.z>0? IOR: 1/IOR;
+		float r = reflectivity(fabs(wo.z), ior);
+
+		if (r == 1 || sampler.get1f()<r) // reflected
+		{
 			wi = vec3f(-wo.x, -wo.y, wo.z);
 			pdf = r;
 			return r / fabs(wi.z);
 		}
-		else {
-			wi = vec3f(-wo.x/ior, -wo.y/ior, -wo.z*cost/cosi);
+		else // transmitted
+		{
+			wi = vec3f(-wo.x/ior, -wo.y/ior, 0);
+			wi.z = sqrtf(fmax(0, 1 - wi.x*wi.x - wi.y*wi.y));
+			if (wo.z > 0) wi.z *= -1;
 			pdf = 1-r;
 			return (1-r) / (fabs(wi.z) * ior * ior);
-			// the 1/ior^2 coefficient is used to convert energy to radiance.
-			// it might not make a difference if light sources and camera are in media of the same IOR
+			// the 1/ior^2 coefficient is used to convert energy to radiance
+			// see note_refract_radiance
 		}
 	}
 };
