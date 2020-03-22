543fcc0a06cda04ad6d6632f789a1591fe74a9f6
bilibili <fenggaobuke@gmail.com>
2020-03-22 23:27:19 +0800

    change reflectivity interface

--- src/bsdfs/dielectric.hpp
+++ src/bsdfs/dielectric.hpp
@@ -16,21 +16,26 @@ public:
 	}
 
 	// Fresnel's law for dielectrics; cosi>0
-	static inline float reflectivity(float cosi, float ior)
+	static inline float reflectivity(float cosi, float& cost, float ior)
 	{
-		float sini = sqrtf(1-cosi*cosi);
+		float sini = sqrtf(fmax(0,1-cosi*cosi));
 		float sint = sini / ior;
 		if (sint >= 1) return 1; // total reflect
-		float cost = sqrtf(1-sint*sint);
+		cost = sqrtf(1-sint*sint);
 		float r1 = (ior*cosi - cost) / (ior*cosi + cost);
 		float r2 = (cosi - ior*cost) / (cosi + ior*cost);
 		return 0.5 * (r2*r2 + r1*r1);
+	}
+	static inline float reflectivity(float cosi, float ior) {
+		float cost;
+		return reflectivity(cosi, cost, ior);
 	}
 
 	Color sample_f(const vec3f& wo, vec3f& wi, float& pdf, Sampler& sampler) const override
 	{
 		float ior = wo.z>0? IOR: 1/IOR;
-		float r = reflectivity(fabs(wo.z), ior);
+		float cost;
+		float r = reflectivity(fabs(wo.z), cost, ior);
 
 		if (r == 1 || sampler.get1f()<r) // reflected
 		{
@@ -40,9 +45,7 @@ public:
 		}
 		else // transmitted
 		{
-			wi = vec3f(-wo.x/ior, -wo.y/ior, 0);
-			wi.z = sqrtf(fmax(0, 1 - wi.x*wi.x - wi.y*wi.y));
-			if (wo.z > 0) wi.z *= -1;
+			wi = vec3f(-wo.x/ior, -wo.y/ior, -wo.z * cost/fabs(wo.z));
 			pdf = 1-r;
 			return (1-r) / (fabs(wi.z) * ior * ior);
 			// the 1/ior^2 coefficient is used to convert energy to radiance
